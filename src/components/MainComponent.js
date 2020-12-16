5eaeac8fc90ec755b56a258fd12a0bca060165a2
NouhaylaRhn <nouhayla.erryhany@gmail.com>
2020-12-16 20:07:10 +0000

    adding controlled form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,12 +6,12 @@ import {DISHES} from '../shared/dishes' ;
 import {PROMOTIONS} from '../shared/promotions' ;
 import {LEADERS} from '../shared/leaders' ;
 import {COMMENTS} from '../shared/comments' ;
-
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import {Switch , Route , Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 
 
@@ -41,12 +41,23 @@ class Main extends Component {
         <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
               promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
               leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-        
-        
         />
 
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish= {this.state.dishes.filter((dish)  => dish.id ===  parseInt(match.params.dishId, 10))[0] }
+                    comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))[0]} 
+        />
+      );
+    }
+
+    const AboutUs = () => {
+      return <About leaders={this.state.leaders}></About>;
+    }
+
     return (
       <div className="App">
 
@@ -55,7 +66,9 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage}/> 
           <Route exct path="/menu" component={ () => <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={AboutUs} />
           <Redirect to="/home"/> 
         </Switch>
         <Footer />
