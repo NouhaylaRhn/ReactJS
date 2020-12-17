2844005e06e8355d0cfb7fefcde82c3753ec3439
NouhaylaRhn <nouhayla.erryhany@gmail.com>
2020-12-17 12:19:23 +0000

    introducing redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron, NavbarToggler, Collapse, NavItem, Nav } from 'reactstrap';
+import { Navbar, NavbarBrand, Jumbotron, NavbarToggler, Collapse, NavItem, Nav , Button, Modal, ModalBody, ModalHeader, FormGroup , Form, Input, Label} from 'reactstrap';
 import {NavLink} from 'react-router-dom'; 
 
 
@@ -10,9 +10,16 @@ class Header extends Component {
         super(props);
     
         this.toggleNav = this.toggleNav.bind(this);
+
         this.state = {
-          isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+
+
       }
 
       toggleNav() {
@@ -20,6 +27,24 @@ class Header extends Component {
           isNavOpen: !this.state.isNavOpen
         });
       }
+
+      toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
+
+      handleLogin(event){
+          this.toggleModal();
+          alert("Username: " +this.username.value+ "Password:" + this.password.value + "Remember:" +this.remember.checked);
+          event.preventDefault();
+      }
+
+
+
+
+
+      
 
     render() {
         return(
@@ -43,6 +68,12 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -56,6 +87,37 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                
+                <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+
+                    </ModalBody>
+                      
+                
+
+                </Modal>
+                
             </div>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,9 +9,25 @@ import {COMMENTS} from '../shared/comments' ;
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {Switch , Route , Redirect } from 'react-router-dom';
+import {Switch , Route , Redirect, withRouter } from 'react-router-dom';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import {connect } from 'react-redux';
+
+
+
+const MapStateToProps = state => {
+  return {
+    dishes : state.dishes,
+    comments : state.commnts,
+    promotions : state.promotions,
+    leaders : state.leaders
+
+  }
+    
+
+  
+}
 
 
 
@@ -38,20 +54,20 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-        />
+        <Home 
+              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
 
       );
     }
 
     const DishWithId = ({match}) => {
       return(
-        <DishDetail dish= {this.state.dishes.filter((dish)  => dish.id ===  parseInt(match.params.dishId, 10))[0] }
-                    comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))[0]} 
-        />
-      );
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
     }
 
     const AboutUs = () => {
@@ -78,4 +94,4 @@ class Main extends Component {
  
 }
 
-export default Main;
+export default  withRouter(connect(MapStateToProps)(Main));
